0cca9ee2444f220144554984a3f3caff8de67c04
“malthaczar” <malthaczar@yahoo.com>
2015-09-23 21:29:33 -0400

    css changes and logo

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 10,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(29.6270, -82.3730), // Gainesville
